f1c79490b23b73703b972ad2cc8d24bf557aee39
Roy Wenang R <roywenangr@gmail.com>
2024-06-24 22:34:23 +0700

    Update diabetes_app.py

--- diabetes_app.py
+++ diabetes_app.py
@@ -72,12 +72,12 @@ age = col3.selectbox(
 # 4. HighChol
 highchol = col1.selectbox(
     "4. Kolesterol Tinggi: Pernahkah Anda diberitahu oleh dokter, perawat, atau profesional kesehatan lainnya bahwa Kolesterol Darah Anda tinggi?",
-    ('Yes', 'No'), index=1)
+    ('Ya', 'Tidak'), index=1)
 
 # 5. HighBP
 highbp = col2.selectbox(
     "5. Tekanan Darah Tinggi: Pernahkah Anda diberitahu oleh dokter, perawat, atau profesional kesehatan lainnya bahwa Anda memiliki Tekanan Darah Tinggi?",
-    ('Yes', 'No'), index=0)
+    ('Ya', 'Tidak'), index=0)
 
 # 6. GenHlth
 genhlth = col3.selectbox("6. Kesehatan Umum: Bagaimana Anda memberi peringkat Kesehatan Umum Anda dalam skala dari 1 = Sangat Baik hingga 5 = Buruk? Pertimbangkan kesehatan fisik dan mental.",
@@ -128,9 +128,9 @@ def prep_df(df):
     df['Age'] = df['Age'].replace({'Age 18 to 24': 1, 'Age 25 to 29': 2, 'Age 30 to 34': 3, 'Age 35 to 39': 4, 'Age 40 to 44': 5, 'Age 45 to 49': 6,
                                    'Age 50 to 54': 7, 'Age 55 to 59': 8, 'Age 60 to 64': 9, 'Age 65 to 69': 10, 'Age 70 to 74': 11, 'Age 75 to 79': 12, 'Age 80 or older': 13})
     # HighChol
-    df['HighChol'] = df['HighChol'].replace({'Yes': 1, 'No': 0})
+    df['HighChol'] = df['HighChol'].replace({'Ya': 1, 'Tidak': 0})
     # HighBP
-    df['HighBP'] = df['HighBP'].replace({'Yes': 1, 'No': 0})
+    df['HighBP'] = df['HighBP'].replace({'Ya': 1, 'Tidak': 0})
     # GenHlth
     df['GenHlth'] = df['GenHlth'].replace(
         {'Sempurna':1, 'Sangat Baik':2, 'Baik':3, 'Cukup':4, 'Kurang':5})
